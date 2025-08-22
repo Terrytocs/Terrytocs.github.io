407c111a10f2c6a40af6460161f7d0e4f36fb3ac
Theodore Scott-Henry <64044647+Terrytocs@users.noreply.github.com>
2025-08-22 00:57:02 -0500

    Add files via upload

--- html/Website/main.js
+++ html/Website/main.js
@@ -6,10 +6,25 @@ function mainFn(){
   toggleHamFn();
 }
 
-function toggleHamFn(){
-  const hamBar=document.getElementById('_ham_svg');
-  
-  hamBar.addEventListener('click',()=>{
-    hamBar.toggleAttribute('active');
-  })
+function toggleHamFn(id){
+  const ham_bar=document.getElementById('_ham_svg');
+
+  ham_bar.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    ham_bar.classList.toggle('active');
+  },{capture:true});
+
+  window.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('nav__link')){
+      ham_bar.classList.remove('active');
+    }else if(!e.target){
+      return;
+    }else{
+      ham_bar.classList.remove('active')
+    }
+  });
 }
+
+  
+
+  

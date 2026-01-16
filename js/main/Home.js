a6ae64e2a426c11121dc239d4e9e6e39ff070648
Theodore Scott-Henry <64044647+Terrytocs@users.noreply.github.com>
2026-01-16 12:32:11 -0500

    Add files via upload

--- js/main/Home.js
+++ js/main/Home.js
@@ -1,5 +1,7 @@
 ("use strict");
 
+var temp_key = "temp_f90870962aefb4abad0046d2aac0e1b2";
+
 class BibleVerse {
   constructor(bookID, chapterID, verseID, textID) {
     const book_container = document.getElementById(bookID);
@@ -34,7 +36,7 @@ class AnimeNews {
     const anime_synopsis = document.getElementById(synopsisId);
     const anime_list_xhr = new XMLHttpRequest();
     anime_list_xhr.open("GET", "https://proxy.cors.sh/https://cdn.animenewsnetwork.com/encyclopedia/reports.xml?type=anime&id=155&nskip=0&nlist=all");
-    anime_list_xhr.setRequestHeader("x-cors-api-key", "temp_573b826a0a1640d95cccbf53cea4236e");
+    anime_list_xhr.setRequestHeader("x-cors-api-key", temp_key);
     anime_list_xhr.responseType = "text";
     anime_list_xhr.addEventListener("load", () => {
       if (anime_list_xhr.status === 200) {
@@ -43,22 +45,21 @@ class AnimeNews {
         const anime_xhr = new XMLHttpRequest();
         const anime_id = anime_id_list[Math.floor(anime_id_list.length * Math.random())];
         anime_xhr.open("GET", `https://proxy.cors.sh/https://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=${anime_id}`);
-        anime_xhr.setRequestHeader("x-cors-api-key", "temp_573b826a0a1640d95cccbf53cea4236e");
+        anime_xhr.setRequestHeader("x-cors-api-key", temp_key);
         anime_xhr.responseType = "text";
         anime_xhr.addEventListener("load", () => {
           if (anime_xhr.status === 200) {
-            console.log(anime_xhr.responseText);
             const img_src = anime_xhr.responseText.match(/(?<=img src=").*?\.jpg/);
             const title = anime_xhr.responseText.match(/(?<=type="Main title" lang=".*?">).*?(?=<)/);
             const altTitle = anime_xhr.responseText.match(/(?<=type="Alternative title" lang=".*?">).*?(?=<)/);
             const rating = anime_xhr.responseText.match(/(?<=<ratings .*? weighted_score=").*?(?=")/);
             const synopsis = anime_xhr.responseText.match(/(?<=type="Plot Summary">).*?(?=<)/);
             anime_image.src = img_src[0];
+            anime_image.classList.add("img");
             anime_title.innerText = title[0];
             anime_alt_title.innerText = altTitle ? altTitle[0] : "Alternate title unavailable";
             anime_rating.innerText = rating ? parseFloat(rating[0]).toPrecision(2) : "Rating unavailable";
             anime_synopsis.innerHTML = `${synopsis ? synopsis[0] : "Summary unavailable"}<sub class="courtesy-sub">Courtesy of <a class="link" href="https://animenewsnetwork.com">animenewsnetwork.com</a></sub>`;
-            console.log(title, altTitle, rating, synopsis);
           } else {
             anime_xhr.send();
           }
--- js/main/Nav.js
+++ js/main/Nav.js
@@ -3,11 +3,17 @@
 class Nav {
   constructor(id) {
     this.setup(id);
+    this.setupHambar();
   }
   setup(id) {
     const nav = document.getElementById(id);
     if (id === "_home_nav") {
       nav.innerHTML = `
+      <button id="_hambar" class="hambar">
+      <div ></div>
+      <div ></div>
+      <div ></div>
+      </button>
         <ul>
             <li><a href="./" data-active="">Home</a></li>
             <li><a href="./projects">Projects</a></li>
@@ -18,15 +24,14 @@ class Nav {
     `;
       return null;
     }
-    const ids = [
-      "_home_link",
-      "_projects_link",
-      "_games_link",
-      "_blog_link",
-      "_saq_link",
-    ];
+    const ids = ["_home_link", "_projects_link", "_games_link", "_blog_link", "_saq_link"];
 
     nav.innerHTML = `
+    <button id="_hambar" class="hambar">
+    <div ></div>
+      <div ></div>
+      <div ></div>
+    </button>
         <ul>
             <li><a id="${ids[0]}" href="../">Home</a></li>
             <li><a id="${ids[1]}" href="../projects">Projects</a></li>
@@ -37,12 +42,17 @@ class Nav {
     `;
 
     for (let i = 0; i < ids.length; ++i) {
-      console.log(ids[i].split("_"));
       if (ids[i].split("_")[1] === id.split("_")[1]) {
         const activePage = document.getElementById(ids[i]);
         activePage.setAttribute("data-active", "");
       }
     }
     return null;
+  }
+  setupHambar() {
+    var hambar = document.getElementById("_hambar");
+    hambar.addEventListener("click", function (e) {
+      this.classList.toggle("active");
+    });
   }
 }
